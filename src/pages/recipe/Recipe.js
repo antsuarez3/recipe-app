66b77e36d4aa65165d554f2202bf41c629b0abb4
Anthony Suarez <antsuarez305@gmail.com>
2022-05-25 19:05:32 -0500

    Create Ingredient Page

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -18,9 +18,9 @@ function Recipe() {
           <h2 className='page-title'>{recipe.title}</h2>
           <p>Takes {recipe.cookingTime} to cook.</p>
           <ul>
-            {recipe.ingredients.map((ing) => 
+            {recipe.ingredients.map((ing) => (
               <li key={ing}>{ing}</li>
-            )}
+            ))}
           </ul>
           <p className='method'>{recipe.method}</p>
         </>
